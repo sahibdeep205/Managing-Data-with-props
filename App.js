dd3e8fc2d11413a4490baf597b898e59da5fe88e
sahibdeep205 <sahibdeep.kalyan@arcadiaschool.ae>
2026-04-18 18:47:45 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tree from './App.js';
+
+ReactDOM.render(<Tree />, document.getElementById('root'));
---
+++ App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Student() {
+    return <h2>Hi, I am Codingal Student</h2>;
+}
+
+ReactDOM.render(<Student />, document.getElementById('root'));
